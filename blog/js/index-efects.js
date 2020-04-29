68f46ca9809e14d52d5a200afd64e8b7ad892ed0
DavidCaba6194 <cabayosoyasi@gmail.com>
2020-04-29 19:24:14 +0200

    Leer JSON posts

--- blog/js/index-efects.js
+++ blog/js/index-efects.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
 
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'blog/json/posts.json', true);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            let posts = JSON.parse(this.responseText);
+
+            for(let item of posts) {
+                console.log(item.id);
+                console.log(item.titulo);
+                console.log(item.descripcion);
+                console.log(item.imagen);
+                console.log(item.url);
+            }
+        }
+    }
 });
